cc9b398f30d948e37219667f5c5029a652e27513
AshOil <ka0@kakao.com>
2020-08-02 12:41:14 +0900

    0802 first

--- D2/2001.py
+++ D2/2001.py
@@ -2,10 +2,56 @@
 
 
 import sys
-sys.stdin = open('input_data/1984.txt',"r")
+sys.stdin = open('input_data/2001.txt',"r")
 
 
 T = int(input())
 
 for t in range(1, T+1):
-    numbers = list(map(int, input() .split()))1
+    N, M = list(map(int, input() .split()))
+    fly_numbers = []
+    for _ in range(N):
+        fly_number = list(map(int, input() .split()))
+        fly_numbers.append(fly_number)
+    sum_flys = []
+    sum_fly = 0
+    hori_start = 0
+    hori_adjust = M
+    verti_start = 0
+    verti_adjust = M
+
+    for turn_verti in range(N-M+1):
+        hori_start = 0
+        hori_adjust = M
+        for turn in range(N-M+1):
+            sum_fly = 0
+            for hori in fly_numbers[hori_start:hori_adjust]:
+                sum_fly += sum(hori[verti_start:verti_adjust])
+            sum_flys.append(sum_fly)
+            hori_adjust += 1
+            hori_start += 1
+        verti_adjust += 1
+        verti_start += 1
+    print('#{} {}'.format(t, max(sum_flys)))
+    
+
+
+
+
+
+
+
+
+
+    # ### 세로 더하기 ##
+    # for turn in range(N-M+1):
+    #     sum_fly = 0
+    #     for hori in fly_numbers[hori_start:hori_adjust]:
+    #         print(hori)
+    #         sum_fly += sum(hori[verti_start:verti_adjust])
+    #     sum_flys.append(sum_fly)
+    #     hori_adjust += 1
+    #     hori_start += 1
+    # print(sum_flys)
+
+
