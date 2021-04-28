764940564e998caac4c693a966efc37f8a5401ab
AshOil <deckloc603@gmail.com>
2021-04-29 03:21:48 +0900

    0429 APS

---
+++ Baekjoon/Gold/Gold4/boj_2141_우체국.py
@@ -0,0 +1,21 @@
+#TODO 파트 7
+
+import sys; sys.stdin = open('input_data/2141.txt')
+town = int(input())
+town_list = []
+for _ in range(town):
+    town_list.append(list(map(int, input().split())))
+
+min_data = 1000000
+for i in range(town_list[0][0], town_list[town-1][0]+1):
+    check = 0
+    for data in town_list:
+        check += data[1] * (max(data[0],i) - min(data[0],i))
+    if check < min_data:
+        min_data = check
+    else:
+        print(i-1)
+        break
+
+else:
+    print(town_list[town-1][0])
