5c46cc04a80e9e3066f49b9383e12065812576cc
AshOil <ka0@kakao.com>
2021-05-27 20:35:42 +0900

    0527 APS

--- Programmers/Level3/prog_여행경로.py
+++ Programmers/Level3/prog_여행경로.py
@@ -1,7 +1,5 @@
-## TODO 내일 다시!
-
 from copy import deepcopy
-tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+tickets = [["ICN", "A"], ["ICN", "B"], ["B", "ICN"]]
 
 detartures = {"ICN" : 0}
 translate = ["ICN"]
@@ -15,9 +13,9 @@ def DFS(x, route):
             routes.append(deepcopy(route))
     for i in range(N):
         if Ground[x][i]:
-            Ground[x][i] = 0
+            Ground[x][i] -= 1
             DFS(i, route)
-            Ground[x][i]= 1
+            Ground[x][i] += 1
     route.pop()
 
 for ticket in tickets:
@@ -25,12 +23,16 @@ for ticket in tickets:
         detartures[ticket[0]] = count
         translate.append(ticket[0])
         count+=1
+    if ticket[1] not in detartures.keys():
+        detartures[ticket[1]] = count
+        translate.append(ticket[1])
+        count+=1
 N = len(detartures)
 
 Ground = [[0]*N for _ in range(N)]
 for ticket in tickets:
-    Ground[detartures[ticket[0]]][detartures[ticket[1]]] = 1
+    Ground[detartures[ticket[0]]][detartures[ticket[1]]] += 1
 
 DFS(0, [])
-# result = sorted(routes, key=lambda x : (x[0], x[1], x[2]))
-print(routes)
+result = sorted(routes, key=lambda x : [x[i] for i in range(len(routes[0]))])
+print(result[0])
