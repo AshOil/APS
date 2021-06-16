b769596e386cd826530ed8ea6ab6508984b6fe15
AshOil <ka0@kakao.com>
2021-06-16 16:57:59 +0900

    0616 APS

--- Programmers/Level2/prog_점프와순간이동.py
+++ Programmers/Level2/prog_점프와순간이동.py
@@ -1,26 +1,9 @@
-
-N = 5000
-from collections import deque
-
-
-
 def solution(N):
-    lets_go = deque()
-    lets_go.append([1, 1])
-    min_count = 100
-    while lets_go:
-        num, count = lets_go.popleft()
-        if num > N:
-            continue
-        if count > min_count:
-            continue
-        if num == N:
-            if count < min_count:
-                min_count = count
-        if num*2 <= N:
-            lets_go.append([num*2, count])
-        lets_go.append([num+1, count + 1])
-    return min_count
-
-print(solution(N))
-
+    result = 1
+    while N != 1:
+        if N%2:
+            N -=1
+            result += 1
+        else:
+            N /= 2
+    return result
